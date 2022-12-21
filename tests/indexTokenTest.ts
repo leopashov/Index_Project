1063b8f30ca838b181be9ae4ef21c505475a38a1
leo <lp916@ic.ac.uk>
2022-12-21 16:10:30 +0200

    Standard ERC-20 testing

--- tests/indexTokenTest.ts
+++ tests/indexTokenTest.ts
@@ -11,14 +11,11 @@ describe("Index Token Contract", async() => {
 
     beforeEach(async () =>{
         [deployer, acc1] = await ethers.getSigners();
-        // console.log("deployer address: ",deployer.address, "acc1 address: ", acc1.address)
-        
+                
         const indexTokenFactory = await ethers.getContractFactory('BitwiserIndex');
         indexTokenContract = await indexTokenFactory.connect(deployer).deploy();
         await indexTokenContract.deployed();
-        // console.log("Index token address: ", indexTokenContract.address)
-        // console.log("minter role address: ", await indexTokenContract.MINTER_ROLE())
-          
+                  
     })
 
     describe("When the token is first deployed", async () => {
@@ -33,14 +30,28 @@ describe("Index Token Contract", async() => {
 
             expect(deployerHasMinterRole).to.eq(true);
         })
+        it("Has the deployer address as the default admin role", async () => {
+            const defaultAdminRoleHash = indexTokenContract.DEFAULT_ADMIN_ROLE();
+            const deployerAddress = deployer.address;
+            const deployerHasAdminRole = await indexTokenContract.hasRole(defaultAdminRoleHash, deployerAddress);
+
+            expect(deployerHasAdminRole).to.eq(true);
+        })
+        it("Has the deployer address as the pauser role", async () => {
+            const pauserRoleHash = indexTokenContract.PAUSER_ROLE();
+            const deployerAddress = deployer.address;
+            const deployerHasPauserRole = await indexTokenContract.hasRole(pauserRoleHash, deployerAddress);
+
+            expect(deployerHasPauserRole).to.eq(true);
+        })
     })
 
     describe("When the deployer calls mint and burn functions", async () => {
 
         it("mints tokens, increasing token total supply", async () => {
             const initialTokenSupply = await indexTokenContract.totalSupply();
-
-            const tokensToMint = Math.floor(Math.random() * 100);
+            // generate random number between 10 and 100
+            const tokensToMint = Math.floor(Math.random() * 91) + 10;
             console.log(tokensToMint)
             const tokensToMintBN = ethers.utils.parseEther(String(tokensToMint));
             const mintTx = await indexTokenContract.connect(deployer).mint(indexTokenContract.address, tokensToMintBN);
@@ -48,6 +59,53 @@ describe("Index Token Contract", async() => {
             const finalTokenSupply = await indexTokenContract.totalSupply();
             const expectedFinalTokenSupply = initialTokenSupply.add(tokensToMintBN)
             expect(finalTokenSupply).to.eq(expectedFinalTokenSupply)
+        })
+        it("burns tokens, decreasing token total supply", async () => {
+            // Mint Tokens to deployer address
+            const tokensToMint = Math.floor(Math.random() * 91) + 10;
+            console.log("Minting ", tokensToMint, "tokens")
+            const tokensToMintBN = ethers.utils.parseEther(String(tokensToMint));
+            const mintTx = await indexTokenContract.connect(deployer).mint(deployer.address, tokensToMintBN);
+            await mintTx.wait();
+            
+            const initialTokenSupply = await indexTokenContract.totalSupply();
+            console.log("Initial token supply (BN): ", initialTokenSupply)
+            const tokensToBurn = Math.floor(Math.random() * 10);
+            console.log("Burning ", tokensToBurn, "tokens")
+            // Burn tokens (from deployer - burn burns from msg.sender)
+            const tokensToBurnBN = ethers.utils.parseEther(String(tokensToBurn));
+            const BurnTx = await indexTokenContract.connect(deployer).burn(tokensToBurnBN);
+            await BurnTx.wait();
+            const finalTokenSupply = await indexTokenContract.totalSupply();
+            const expectedFinalTokenSupply = initialTokenSupply.sub(tokensToBurnBN)
+            expect(finalTokenSupply).to.eq(expectedFinalTokenSupply)
+        })
+    })
+    describe("When acc1 calls mint and burn functions", async () => {
+        it("Reverts the mint transaction", async () => {
+            const initialTokenSupply = await indexTokenContract.totalSupply();
+            console.log("initial token supply: ", initialTokenSupply);
+            const tokensToMint = Math.floor(Math.random() * 91) + 10;
+            console.log(tokensToMint);
+            const tokensToMintBN = ethers.utils.parseEther(String(tokensToMint));
+
+            expect(indexTokenContract.connect(acc1).mint(indexTokenContract.address, tokensToMintBN)).to.be.reverted
+        })
+
+        it("Reverts the burn transaction", async () => {
+            // Mint Tokens to acc1 address
+            const tokensToMint = Math.floor(Math.random() * 91) + 10;
+            console.log("Minting ", tokensToMint, "tokens")
+            const tokensToMintBN = ethers.utils.parseEther(String(tokensToMint));
+            const mintTx = await indexTokenContract.connect(deployer).mint(acc1.address, tokensToMintBN);
+            await mintTx.wait();
+            const initialTokenSupply = await indexTokenContract.totalSupply();
+            console.log("initial token supply: ", initialTokenSupply);
+            const tokensToBurn = Math.floor(Math.random() * 10);
+            console.log(tokensToBurn);
+            const tokensToBurnBN = ethers.utils.parseEther(String(tokensToBurn));
+
+            expect(indexTokenContract.connect(acc1).burn(tokensToBurnBN)).to.be.reverted
         })
     })
 })
